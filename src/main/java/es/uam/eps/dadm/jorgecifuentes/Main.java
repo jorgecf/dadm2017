a287227aef1c9bf6c99b0bf56e2a747e910c9cca
Jorge Cifuentes <Jorge.cifuentes95@gmail.com>
2017-02-01 10:26:20 +0100

    Configuracion android studio

---
+++ src/main/java/es/uam/eps/dadm/jorgecifuentes/Main.java
@@ -0,0 +1,13 @@
+package es.uam.eps.dadm.jorgecifuentes;
+
+
+/**
+ * Created by jorgecf on 1/02/17.
+ */
+
+public class Main {
+
+    public static void main(String [ ] args) {
+        System.out.println("Funciona");
+    }
+}
